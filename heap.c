df44aa2a075b10ca91d788c0e4a9ce33beccf35b
LineWelder <redalerm@gmail.com>
2021-10-17 13:16:00 +0200

    Added HeapChunkList structure

--- heap.c
+++ heap.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,22 +15,37 @@ typedef struct
     size_t size;
 } HeapChunk;
 
+typedef struct
+{
+    HeapChunk* chunks;
+    size_t count;
+    size_t capacity;
+} HeapChunkList;
+
 char heap[HEAP_CAPACITY];
 
 HeapChunk allocated_chunks[HEAP_CHUNK_CAPACITY];
-size_t allocated_chunks_count = 0;
+HeapChunkList allocated_chunks_list = {
+    .chunks   = allocated_chunks,
+    .count    = 0,
+    .capacity = HEAP_CHUNK_CAPACITY
+};
 
 HeapChunk freed_chunks[HEAP_CHUNK_CAPACITY + 1] = { { .start = heap, .size = HEAP_CAPACITY } };
-size_t freed_chunks_count = 0;
+HeapChunkList freed_chunks_list = {
+    .chunks   = freed_chunks,
+    .count    = 1,
+    .capacity = HEAP_CHUNK_CAPACITY + 1
+};
 
-void print_chunk_array(const HeapChunk* array, size_t size)
+void print_chunk_list(HeapChunkList list)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < list.count; i++)
     {
         printf(
             "{ .start = %p, .size = %u }\n",
-            array[i].start,
-            array[i].size
+            list.chunks[i].start,
+            list.chunks[i].size
         );
     }
 }
@@ -38,7 +54,7 @@ void* heap_alloc(size_t size)
 {
     assert(size > 0);
 
-    for (int i = 0; i < freed_chunks_count; i++)
+    for (int i = 0; i < freed_chunks_list.count; i++)
     {
         if (freed_chunks[i].size >= size)
         {
@@ -59,7 +75,8 @@ void heap_free(void* ptr)
 int main(void)
 {
     int* num = (int*)heap_alloc(4);
-    print_chunk_array(allocated_chunks, allocated_chunks_count);
-    
+    print_chunk_list(allocated_chunks_list);
+    print_chunk_list(freed_chunks_list);
+
     return 0;
 }
